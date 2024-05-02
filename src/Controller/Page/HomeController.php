36b049c8abf319f768a73dbe2577d8612d4af4ba
Clément GROHAN <clement.grohan@mes-services.me>
2024-05-02 14:56:48 +0200

    Déclaration de la méthode qui servira à afficher les détails des articles pour l'utilisateur

--- src/Controller/Page/HomeController.php
+++ src/Controller/Page/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Page;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
 class HomeController extends AbstractController
 {
@@ -12,5 +13,11 @@ class HomeController extends AbstractController
     public function home(): Response
     {
         return $this->render('Page/index.html.twig');
+    }
+
+    #[Route('/article/{slug}', name: 'article.show', requirements: ['slug' => Requirement::ASCII_SLUG])]
+    public function show(): Response
+    {
+        return $this->render('Page/article.html.twig');
     }
 }
